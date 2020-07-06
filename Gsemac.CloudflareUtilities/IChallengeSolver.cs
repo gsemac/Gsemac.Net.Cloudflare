26af0a154cc74835240e35011108d4071b804d11
gsemac <gabe.semac@gmail.com>
2020-07-06 14:19:01 -0600

    Moved web driver challenge solvers to separate project

--- Gsemac.CloudflareUtilities/IChallengeSolver.cs
+++ Gsemac.CloudflareUtilities/IChallengeSolver.cs
@@ -1,10 +1,4 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
-using System;
-using System.Linq;
-
-namespace Gsemac.CloudflareUtilities {
+﻿namespace Gsemac.CloudflareUtilities {
 
     public interface IChallengeSolver {
 
