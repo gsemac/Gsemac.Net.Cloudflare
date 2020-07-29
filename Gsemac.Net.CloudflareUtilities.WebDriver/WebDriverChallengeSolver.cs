ded297b11c8a8a5209350d52b8e4b7c6469f8029
gsemac <gabe.semac@gmail.com>
2020-07-29 02:50:21 -0600

    Added generic WebDriverChallengeSolver class

---
+++ Gsemac.Net.CloudflareUtilities.WebDriver/WebDriverChallengeSolver.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace Gsemac.Net.CloudflareUtilities.WebDriver {
+
+    public class WebDriverChallengeSolver :
+          WebDriverChallengeSolverBase {
+
+        // Public members
+
+        public WebDriverChallengeSolver(IWebDriver webDriver, WebDriverChallengeSolverOptions options) :
+            base(options) {
+
+            this.webDriver = webDriver;
+
+        }
+
+        // Private members
+
+        protected override IWebDriver CreateWebDriver(WebDriverChallengeSolverOptions options, Uri uri) {
+
+            return webDriver;
+
+        }
+
+        // Private members
+
+        private readonly IWebDriver webDriver;
+
+    }
+
+}
