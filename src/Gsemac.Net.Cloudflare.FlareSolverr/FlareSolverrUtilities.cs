9f0de4664f98366ce31816281849d2108dc8db32
gsemac <gabe.semac@gmail.com>
2023-07-25 00:11:57 -0600

    Added additional default FlareSolverr paths

--- src/Gsemac.Net.Cloudflare.FlareSolverr/FlareSolverrUtilities.cs
+++ src/Gsemac.Net.Cloudflare.FlareSolverr/FlareSolverrUtilities.cs
@@ -24,21 +24,27 @@ namespace Gsemac.Net.Cloudflare.FlareSolverr {
                 GetExecutableFilename() :
                 options.FlareSolverrFileName;
 
+            // FlareSolverr may be located at any of the following paths:
+
             // ./flaresolverr.exe
+            // ./flaresolverr/flaresolverr.exe
+            // ./flaresolverr-vx.x.x-windows-xxx/flaresolverr/flaresolverr.exe
+            // ./flaresolverr_windows_x64/flaresolverr/flaresolverr.exe
+            // ./flaresolverr_windows_x64/flaresolver/flaresolverr.exe
+
+            // The "flaresolver" subfolder is a typo that was present in the v3.2.2 release archive.
 
             if (File.Exists(Path.Combine(inDirectoryPath, flareSolverrFileName)))
                 return Path.Combine(inDirectoryPath, flareSolverrFileName);
 
-            // ./flaresolverr/flaresolverr.exe
-            // ./flaresolverr-vx.x.x-windows-xxx/flaresolverr/flaresolverr.exe
-            // ./flaresolverr_windows_x64/flaresolverr/flaresolverr.exe
-
             // Use natural sorting so that newer versions are listed before older versions.
+            // Newer binary releases of Cloudflare (v3.1.0+) do not contain version information, so we still might end up locating an older version.
 
             foreach (string directoryPath in Directory.EnumerateDirectories(inDirectoryPath, "flaresolverr*", SearchOption.TopDirectoryOnly).OrderByDescending(path => path, new NaturalSortComparer())) {
 
                 foreach (string candidateExecutablePath in new[] {
                     Path.Combine(directoryPath, flareSolverrFileName),
+                    Path.Combine(directoryPath, "flaresolver", flareSolverrFileName),
                     Path.Combine(directoryPath, "flaresolverr", flareSolverrFileName),
                 }) {
 
