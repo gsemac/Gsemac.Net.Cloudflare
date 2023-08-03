c1af8a0906e34d5425c17c675ea4efa7d865ea26
gsemac <gabe.semac@gmail.com>
2023-08-03 14:18:57 -0600

    Improved FlareSolverr lookup Candidate FlareSolverr executables are now sorted by their last modified date to determine which one is the newest.

--- src/Gsemac.Net.Cloudflare.FlareSolverr/FlareSolverrUtilities.cs
+++ src/Gsemac.Net.Cloudflare.FlareSolverr/FlareSolverrUtilities.cs
@@ -16,7 +16,7 @@ namespace Gsemac.Net.Cloudflare.FlareSolverr {
 
         internal static string GetExecutablePath(IFlareSolverrOptions options) {
 
-            string inDirectoryPath = string.IsNullOrWhiteSpace(options.FlareSolverrDirectoryPath) ?
+            string flareSolverrDirectoryPath = string.IsNullOrWhiteSpace(options.FlareSolverrDirectoryPath) ?
                 Directory.GetCurrentDirectory() :
                 options.FlareSolverrDirectoryPath;
 
@@ -25,33 +25,45 @@ namespace Gsemac.Net.Cloudflare.FlareSolverr {
                 options.FlareSolverrFileName;
 
             // FlareSolverr may be located at any of the following paths:
-
+            //
             // ./flaresolverr.exe
             // ./flaresolverr/flaresolverr.exe
             // ./flaresolverr-vx.x.x-windows-xxx/flaresolverr/flaresolverr.exe
             // ./flaresolverr_windows_x64/flaresolverr/flaresolverr.exe
             // ./flaresolverr_windows_x64/flaresolver/flaresolverr.exe
-
+            //
             // The "flaresolver" subfolder is a typo that was present in the v3.2.2 release archive.
+            // Later releases corrected this typo, so we should prefer the version that is correctly spelled.
 
-            if (File.Exists(Path.Combine(inDirectoryPath, flareSolverrFileName)))
-                return Path.Combine(inDirectoryPath, flareSolverrFileName);
+            // If there is a FlareSolverr executable directly in the current directory, return it immediately.
+
+            if (File.Exists(Path.Combine(flareSolverrDirectoryPath, flareSolverrFileName)))
+                return Path.Combine(flareSolverrDirectoryPath, flareSolverrFileName);
 
             // Use natural sorting so that newer versions are listed before older versions.
-            // Newer binary releases of Cloudflare (v3.1.0+) do not contain version information, so we still might end up locating an older version.
+            // Note that newer binary releases of Cloudflare (v3.1.0+) do not contain version information in the directory name.
 
-            foreach (string directoryPath in Directory.EnumerateDirectories(inDirectoryPath, "flaresolverr*", SearchOption.TopDirectoryOnly).OrderByDescending(path => path, new NaturalSortComparer())) {
+            DirectoryInfo flareSolverrDirectoryInfo = new DirectoryInfo(flareSolverrDirectoryPath);
 
-                foreach (string candidateExecutablePath in new[] {
-                    Path.Combine(directoryPath, flareSolverrFileName),
-                    Path.Combine(directoryPath, "flaresolver", flareSolverrFileName),
-                    Path.Combine(directoryPath, "flaresolverr", flareSolverrFileName),
-                }) {
+            foreach (string flareSolverrSubDirectoryPath in flareSolverrDirectoryInfo.EnumerateDirectories("flaresolverr*", SearchOption.TopDirectoryOnly)
+                .OrderByDescending(dir => dir.Name, new NaturalSortComparer()) // Sort directories by version number
+                .Select(dir => dir.FullName)) {
 
-                    if (File.Exists(candidateExecutablePath))
-                        return candidateExecutablePath;
+                // Prefer executables with a later last write time.
+                // We can't use the creation time, because for some releases this will be earlier than the last write time.
+                // Another option is to check the "package.json" file for the version number, but this wasn't available for versions prior to v.3.0.0. 
 
+                string candidateExecutablePath = new[] {
+                    Path.Combine(flareSolverrSubDirectoryPath, flareSolverrFileName),
+                    Path.Combine(flareSolverrSubDirectoryPath, "flaresolverr", flareSolverrFileName),
+                    Path.Combine(flareSolverrSubDirectoryPath, "flaresolver", flareSolverrFileName),
                 }
+                .Where(path => File.Exists(path))
+                .OrderByDescending(path => new FileInfo(path).LastWriteTime)
+                .FirstOrDefault();
+
+                if (!string.IsNullOrWhiteSpace(candidateExecutablePath))
+                    return candidateExecutablePath;
 
             }
 
