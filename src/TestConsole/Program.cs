4ec574858049e7ef166ed9c8f95a0e2e65c54770
gsemac <gabe.semac@gmail.com>
2024-04-21 15:36:37 -0500

    Updated nuget packages

---
+++ src/TestConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using Gsemac.IO.Logging;
+using Gsemac.Net;
+using Gsemac.Net.Cloudflare.FlareSolverr;
+using Gsemac.Net.Http;
+using Gsemac.Polyfills.Microsoft.Extensions.DependencyInjection;
+
+namespace TestConsole {
+
+    internal class Program {
+
+        static ServiceProvider CreateServiceProvider() {
+
+            return new ServiceCollection()
+                .AddSingleton<ILogger, ConsoleLogger>()
+                .AddSingleton<IWebClientFactory, WebClientFactory>()
+                .AddSingleton<IFlareSolverrService, FlareSolverrService>()
+                .AddSingleton<WebRequestHandler, FlareSolverrChallengeHandler>()
+                .BuildServiceProvider();
+
+        }
+
+        static void Main(string[] args) {
+
+            using (ServiceProvider serviceProvider = CreateServiceProvider()) {
+
+                IWebClientFactory webClientFactory = serviceProvider.GetRequiredService<IWebClientFactory>();
+
+                using (IWebClient webClient = webClientFactory.Create())
+                    Console.WriteLine(webClient.DownloadString("https://manhuaplus.com/manga/tales-of-demons-and-gods/"));
+
+                using (IWebClient webClient = webClientFactory.Create())
+                    Console.WriteLine(webClient.UploadString("https://manhuaplus.com/manga/tales-of-demons-and-gods/ajax/chapters/", ""));
+
+            }
+
+        }
+
+    }
+
+}
